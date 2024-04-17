2b1e7c93b467fe681f346f0eaaed78ac54d76881
Samvardha <samvardha.khandwe@gmail.com>
2024-04-17 13:42:24 +0530

    Commit

--- script.js
+++ script.js
@@ -11,4 +11,25 @@ document.querySelectorAll('.faqbox').forEach(function(item) {
     });
 });
 
+const dropdownButton = document.querySelector('.btn');
+const dropdownContent = document.querySelector('.dropdown-content');
+console.log(dropdownButton, dropdownContent);
+
+dropdownButton.addEventListener('click', function(event) {
+    event.stopPropagation();
+    dropdownContent.classList.toggle('show');
+});
+
+window.addEventListener('click', function(event) {
+    if (!event.target.matches('.btn')) {
+        if (dropdownContent.classList.contains('show')) {
+            dropdownContent.classList.remove('show');
+        }
+    }
+});
+
+dropdownContent.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
 
